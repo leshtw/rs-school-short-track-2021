76f273b9d89d896fcdc81f95f25baf32a85ea9ec
Alex Boh <cmextw2610@yandex.ru>
2021-04-11 20:39:30 +0300

    fix: fix task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let j = i + 1;
+    let count = 1;
+    for (j; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        count += 1;
+      } else {
+        break;
+      }
+    }
+    i += count - 1;
+    if (count > 1) {
+      result += `${count}${str[i]}`;
+    } else {
+      result += `${str[i]}`;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
