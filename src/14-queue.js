770d02ec0175507c02d1e4d7d611367984f2e694
Alex Boh <cmextw2610@yandex.ru>
2021-04-12 22:31:55 +0300

    fix: fix task

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,36 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.newList = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    const getLength = (node, count = 0) => ((node.next) ? getLength(node.next, count + 1) : count);
+
+    return getLength(this.newList);
+  }
+
+  enqueue(element) {
+    const newNode = new ListNode(element);
+    if (!this.newList) {
+      this.newList = newNode;
+    } else {
+      let lastNode = this.newList;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    if (this.newList) {
+      const newValue = this.newList.value;
+      this.newList = this.newList.next;
+      return newValue;
+    }
+    return null;
   }
 }
 
